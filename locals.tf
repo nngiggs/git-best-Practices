a6f189dc6530a4f364f75005ebd99c4cea45121c
Gideon Nfor <nngiggs2012@gmail.com>
2022-12-13 13:39:38 -0500

    edited locals

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   common_tags={
-    company = "jjtech"
-    owner = "jjtech DevOps Team"
-    team-email = "team-devops@jjtech.com"
+    company = "gidolitech"
+    owner = "GidOlitech DevOps Team"
+    team-email = "team-devops@Gidolitech.com"
     time = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())    
   }
  
